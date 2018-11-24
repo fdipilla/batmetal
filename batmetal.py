113c47ebaaa7bcd6b563e474865b641df34f384e
fdipilla <fabriziodipilla@gmail.com>
2018-11-24 12:40:27 -0300

    delete obsolete sprites

--- batmetal.py
+++ batmetal.py
@@ -374,13 +374,6 @@ def main():
     cannon_fire_3 = load_image("shoot_3.png", -1)
     cannon_fire = [cannon_fire_0, cannon_fire_1, cannon_fire_2, cannon_fire_3]
 
-    misil_0 = load_image("misil_0.png", -1)
-    misil_1 = load_image("misil_1.png", -1)
-    misil_2 = load_image("misil_2.png", -1)
-    misil_3 = load_image("misil_3.png", -1)
-    misil_4 = load_image("misil_4.png", -1)
-    misil_5 = load_image("misil_5.png", -1)
-    misile_animation = [misil_0, misil_1, misil_2, misil_3, misil_4, misil_5]
     misile_sprite_image = load_image("misile_animation.png",-1)
     misile_sprite_sheet = SpriteSheet(misile_sprite_image)
 
